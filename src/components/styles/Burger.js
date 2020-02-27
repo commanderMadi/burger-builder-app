6aade18b9fe7dfdd2a7d6eb361e39bf185973af1
Darth Coder <00darthcoder00@gmail.com>
2020-02-27 20:45:15 +0200

    adjust burger styling

--- src/components/styles/Burger.js
+++ src/components/styles/Burger.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 // Will refactor to use props later.
 
-const bunWidth = 'width: 430px';
+const bunWidth = 'width: 20rem';
 const bunHeight = 'height: 20px';
-const ingredientWidth = 'width: 420px';
+const ingredientWidth = 'width: 19.8rem';
 const ingredientHeight = 'height: 10px';
 const ingredientBorderRadius = 'border-radius: 1rem';
 const padAndMargin = `
@@ -13,57 +13,58 @@ margin: 2px;
 `;
 
 export const SandwichContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    display: flex;
+    height: 400px;
+    flex-direction: column;
+    align-items: center;
 `;
 
 export const TopBun = styled.div`
-  ${bunWidth};
-  ${bunHeight};
-  ${padAndMargin};
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  background-image: linear-gradient(to right, #a6926c, #ead2a3);
+    ${bunWidth};
+    ${bunHeight};
+    ${padAndMargin};
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    background-image: linear-gradient(to right, #a6926c, #ead2a3);
 `;
 
 export const BottomBun = styled.div`
-  ${bunWidth};
-  ${bunHeight};
-  ${padAndMargin};
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-  background-image: linear-gradient(to left, #a6926c, #ead2a3);
+    ${bunWidth};
+    ${bunHeight};
+    ${padAndMargin};
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    background-image: linear-gradient(to left, #a6926c, #ead2a3);
 `;
 
 export const Meat = styled.div`
-  ${ingredientWidth};
-  ${ingredientHeight};
-  ${ingredientBorderRadius};
-  ${padAndMargin};
-  background-image: linear-gradient(#473433, #522522);
+    ${ingredientWidth};
+    ${ingredientHeight};
+    ${ingredientBorderRadius};
+    ${padAndMargin};
+    background-image: linear-gradient(#473433, #522522);
 `;
 
 export const Salad = styled.div`
-  ${ingredientWidth};
-  ${ingredientHeight};
-  padding: 10px;
-  margin: 2px;
-  background-image: linear-gradient(to left, #82d59f, #38f47a);
+    ${ingredientWidth};
+    ${ingredientHeight};
+    padding: 10px;
+    margin: 2px;
+    background-image: linear-gradient(to left, #82d59f, #38f47a);
 `;
 
 export const Cheese = styled.div`
-  width: 440px;
-  ${ingredientHeight};
-  padding: 10px;
-  margin: 2px;
-  background-image: linear-gradient(to right, #f4dd38, #fff294);
+    ${ingredientWidth};
+    ${ingredientHeight};
+    padding: 10px;
+    margin: 2px;
+    background-image: linear-gradient(to right, #f4dd38, #fff294);
 `;
 
 export const Bacon = styled.div`
-  ${ingredientWidth};
-  height: 1px;
-  padding: 8px;
-  margin: 2px;
-  background-image: linear-gradient(to left, #c25d55, #e6295b);
+    ${ingredientWidth};
+    height: 1px;
+    padding: 8px;
+    margin: 2px;
+    background-image: linear-gradient(to left, #c25d55, #e6295b);
 `;
