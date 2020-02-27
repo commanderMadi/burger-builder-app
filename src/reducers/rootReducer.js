7629bfb0a2ccdab659c28a1b98260ff9ffacd07b
Darth Coder <00darthcoder00@gmail.com>
2020-02-27 20:44:14 +0200

    update root reducer to include orders

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,5 +2,11 @@ import { combineReducers } from 'redux';
 import { authReducer } from './authReducer';
 import { builderReducer } from './builderReducer';
 import { priceReducer } from './priceReducer';
+import { ordersReducer } from './ordersReducer';
 
-export const rootReducer = combineReducers({ auth: authReducer, ingredients: builderReducer, totalPrice: priceReducer });
+export const rootReducer = combineReducers({
+    auth: authReducer,
+    ingredients: builderReducer,
+    totalPrice: priceReducer,
+    orders: ordersReducer
+});
